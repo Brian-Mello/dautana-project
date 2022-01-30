351d912df9d91487993d77197d9b447415d063d3
yure <yure.pereira@codeby.com.br>
2022-01-30 08:02:08 -0300

    fix: remove images from vtex

--- react/PcdCaptalizerModal/index.js
+++ react/PcdCaptalizerModal/index.js
@@ -4,11 +4,10 @@ import React, { useState, useEffect } from "react";
 import styles from "./styles.css";
 
 // images
-import closeIcon from "../assets/close-icon.svg";
 import smileIcon from "../assets/smile-face.svg";
-import audioOn from "../assets/volume-high.svg";
-import audioOff from "../assets/volume-off.svg";
-import CustomInput from './components/customInput';
+import { AudioOn, AudioOff, CloseIcon } from "./icons";
+// import audioOff from "../assets/volume-off.svg";
+import CustomInput from "./components/customInput";
 
 // contants
 import { PAGE_TEXT } from "./contants";
@@ -18,40 +17,48 @@ function PcdCaptalizerModal() {
   const [page, setPage] = useState(1);
   const [isPcd, setIsPcd] = useState(false);
   const [isAudioOn, setIsAudioOn] = useState(false);
-  const [daltonismOption, setDaltonismOption] = useState(false)
-  const [deafnessOption, setDeafnessOption] = useState(false) 
-  const [blindnessOption, setBlindnessOption] = useState(false)
-  const [otherOptions, setOtherOptions] = useState(false)
-  const [textAreaValue, setTextValueArea] = useState("")
-  const [missingInputsError, setMissingInputsError] = useState(false)
-  const [missingTextAreaError, setMissingTextAreaError] = useState(false)
+  const [daltonismOption, setDaltonismOption] = useState(false);
+  const [deafnessOption, setDeafnessOption] = useState(false);
+  const [blindnessOption, setBlindnessOption] = useState(false);
+  const [otherOptions, setOtherOptions] = useState(false);
+  const [textAreaValue, setTextValueArea] = useState("");
+  const [missingInputsError, setMissingInputsError] = useState(false);
+  const [missingTextAreaError, setMissingTextAreaError] = useState(false);
   const localStorageKey = "should-show-acessibility";
 
-  const inputsError = <p className={styles.errorMessage}>Você deve Selecionar alguma necessidade para prosseguir</p>
-  const textAreaErrorMessage = <p className={styles.errorMessage}>Para prosseguir, você precisa preencher as outras necessidades</p>
+  const inputsError = (
+    <p className={styles.errorMessage}>
+      Você deve Selecionar alguma necessidade para prosseguir
+    </p>
+  );
+  const textAreaErrorMessage = (
+    <p className={styles.errorMessage}>
+      Para prosseguir, você precisa preencher as outras necessidades
+    </p>
+  );
 
   useEffect(() => {
     readText(page);
   }, [isAudioOn]);
 
-  const readText = async (page) => {    
-    const textToPlay = PAGE_TEXT[page];   
+  const readText = async (page) => {
+    const textToPlay = PAGE_TEXT[page];
     if (isAudioOn) {
       textToPlay.forEach(async (element, index) => {
         await getNextAudio(element.text);
-        if(index === (textToPlay.length -1)){
-          setIsAudioOn(false)
+        if (index === textToPlay.length - 1) {
+          setIsAudioOn(false);
         }
       });
 
       async function getNextAudio(sentence) {
-        let audio = new SpeechSynthesisUtterance(sentence);        
+        let audio = new SpeechSynthesisUtterance(sentence);
         window.speechSynthesis.speak(audio);
         return new Promise((resolve) => {
           audio.onend = resolve;
         });
       }
-    } else {      
+    } else {
       const audio = window.speechSynthesis;
       audio.cancel();
     }
@@ -59,7 +66,7 @@ function PcdCaptalizerModal() {
 
   useEffect(() => {
     const showModal = window.localStorage.getItem("notShowMore");
-   // readText(1);
+    // readText(1);
     if (!showModal) {
       setIsOpen(true);
     }
@@ -71,12 +78,8 @@ function PcdCaptalizerModal() {
   };
 
   const handleNextPage = () => {
-    if (!isPcd) {
-      setIsPcd(true);
-    }
 
     const data = {
-      isPcd,
       daltonismOption,
       deafnessOption,
       blindnessOption,
@@ -98,26 +101,32 @@ function PcdCaptalizerModal() {
   };
 
   const handleCheckFields = () => {
-    if (!daltonismOption && !deafnessOption && !blindnessOption && !otherOptions) {
-      setMissingInputsError(true)
+    if (
+      !daltonismOption &&
+      !deafnessOption &&
+      !blindnessOption &&
+      !otherOptions
+    ) {
+      setMissingInputsError(true);
       setTimeout(() => {
-        setMissingInputsError(false)
-      }, 3000)
-      return
+        setMissingInputsError(false);
+      }, 3000);
+      return;
     } else if (otherOptions && textAreaValue === "") {
-      setMissingTextAreaError(true)
+      setMissingTextAreaError(true);
       setTimeout(() => {
-        setMissingTextAreaError(false)
-      }, 3000)
-      return
+        setMissingTextAreaError(false);
+      }, 3000);
+      return;
+    } else {
+      handleNextPage();
     }
+  };
 
-    return true;
-  }
   const handleIsPcd = () => {
-    setIsPcd(true);
     handleNextPage();
   };
+  
   const handleAudioClick = () => {
     setIsAudioOn(!isAudioOn);
   };
@@ -134,7 +143,6 @@ function PcdCaptalizerModal() {
       </section>
 
       <section className={styles.fluxeButtonsContainer}>
-
         <button className={styles.modalButton} onClick={() => handleIsPcd()}>
           Sim
         </button>
@@ -144,7 +152,7 @@ function PcdCaptalizerModal() {
         >
           Não
         </button>
-      </section >
+      </section>
 
       <button className={styles.modalButton} onClick={() => handleCloseModal()}>
         Não mostrar novamente
@@ -157,16 +165,35 @@ function PcdCaptalizerModal() {
       <h2>Quais tipos de necessidades você tem?</h2>
 
       <section className={styles.inputsContainer}>
-        <CustomInput name={"Daltonismo"} onChange={(e) => setDaltonismOption(e.target.checked)} />
-        <CustomInput name={"Deficiência Visual"} onChange={(e) => setDeafnessOption(e.target.checked)} />
-        <CustomInput name={"Deficiência Auditiva"} onChange={(e) => setBlindnessOption(e.target.checked)} />
-        <CustomInput name={"Outros"} onChange={(e) => setOtherOptions(e.target.checked)} />
+        <CustomInput
+          name={"Daltonismo"}
+          onChange={(e) => setDaltonismOption(e.target.checked)}
+        />
+        <CustomInput
+          name={"Deficiência Visual"}
+          onChange={(e) => setDeafnessOption(e.target.checked)}
+        />
+        <CustomInput
+          name={"Deficiência Auditiva"}
+          onChange={(e) => setBlindnessOption(e.target.checked)}
+        />
+        <CustomInput
+          name={"Outros"}
+          onChange={(e) => setOtherOptions(e.target.checked)}
+        />
       </section>
 
-      <textarea placeholder='Descreva as outras necessidades...' className={styles.optionTextArea} onChange={(e) => setTextValueArea(e.target.value)} />
+      <textarea
+        placeholder="Descreva as outras necessidades..."
+        className={styles.optionTextArea}
+        onChange={(e) => setTextValueArea(e.target.value)}
+      />
       {missingTextAreaError ? textAreaErrorMessage : ""}
 
-      <button className={styles.modalButton} onClick={() => handleNextPage()}>
+      <button
+        className={styles.modalButton}
+        onClick={() => handleCheckFields()}
+      >
         Avançar
       </button>
       {missingInputsError ? inputsError : ""}
@@ -175,19 +202,15 @@ function PcdCaptalizerModal() {
 
   const thirdModalPage = (
     <>
-      <h2>
-        Gostaria de aplicar a mudanças no site para as suas necessidades?
-      </h2>
+      <h2>Gostaria de aplicar a mudanças no site para as suas necessidades?</h2>
 
       <section className={styles.fluxeButtonsContainer}>
-
         <button className={styles.modalButton} onClick={() => handleNextPage()}>
           Sim
         </button>
         <button className={styles.modalButton} onClick={() => handleNextPage()}>
           Não
         </button>
-
       </section>
       <button
         className={styles.modalButton}
@@ -200,12 +223,12 @@ function PcdCaptalizerModal() {
 
   const lastModalPage = (
     <>
-    <section className={styles.modalTextContainer}>
+      <section className={styles.modalTextContainer}>
         <h2>Agradecemos pela participação!</h2>
         <p>Buscamos melhorar continuamente a acessibilidade no nosso site.</p>
         <p>Esperamos que você tenha uma boa experiência. Boas compras!</p>
       </section>
-      <img src={smileIcon} alt="Smile face" className={styles.smileImage}/>
+      <img src={smileIcon} alt="Smile face" className={styles.smileImage} />
 
       <button className={styles.modalButton} onClick={() => handleCloseModal()}>
         Fechar
@@ -227,15 +250,13 @@ function PcdCaptalizerModal() {
     <div className={styles.modalContainer}>
       <section className={styles.modal}>
         <div className={styles.audioButton} onClick={handleAudioClick}>
-          {" "}
-          <img src={isAudioOn ? audioOn : audioOff} />{" "}
+          {isAudioOn ? <AudioOn /> : <AudioOff />}
         </div>
-        <img
-          src={closeIcon}
-          alt="close icon"
-          className={styles.closeIcon}
-          onClick={() => handleCloseModal()}
-        />
+
+        <div className={styles.closeIcon} onClick={() => handleCloseModal()}>
+          <CloseIcon />
+        </div>
+
         {actualPage}
       </section>
     </div>
